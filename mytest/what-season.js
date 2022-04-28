4072cc4717f19a34734192f366fa6372a21855ea
Andrei Krasko <andrei@krasko.by>
2022-04-28 13:10:02 +0300

    feat: not full what-seasons

---
+++ mytest/what-season.js
@@ -0,0 +1,38 @@
+//const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Extract season from given date and expose the enemy scout!
+ * 
+ * @param {Date | FakeDate} date real or fake date
+ * @returns {String} time of the year
+ * 
+ * @example
+ * 
+ * getSeason(new Date(2020, 02, 31)) => 'spring'
+ * 
+ */
+function getSeason(date) {
+   //throw new NotImplementedError('Not implemented');
+   function isValidDate(value) {
+      let dateWrapper = new Date(value);
+      console.log(value, !isNaN(dateWrapper.getDate()));
+      return (!isNaN(dateWrapper.getDate()));
+   }
+   if (date) {
+      //if (Object.prototype.toString.call(date) === "[object Date]") {
+      if (isValidDate(date)) {
+         let month = date.getMonth();
+         //console.log(month);
+         if (month == 0 || month == 1 || month == 11) return 'winter';
+         else if (month == 2 || month == 3 || month == 4) return 'spring';
+         else if (month == 5 || month == 6 || month == 7) return 'summer'
+         else return 'autumn';
+      } else return new NotImplementedError('Invalid date!')
+   }
+   else return 'Unable to determine the time of year!'
+}
+let input = new Date(1994, 8, 26, 3, 0, 11, 500);
+console.log(getSeason(input));
+/*module.exports = {
+   getSeason
+};*/
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getSeason(date) {
+  //throw new NotImplementedError('Not implemented');
+  function isValidDate(value) {
+    let dateWrapper = new Date(value);
+    //console.log(value, !isNaN(dateWrapper.getDate()));
+    return (!isNaN(dateWrapper.getDate()));
+  }
+  //console.log("isValidDate(date)", isValidDate(date))
+  //console.log("Object.prototype.toString", Object.prototype.toString);
+  let ControlDate = new Date(date)
+  console.log("date.toString", (date.toString.call(date)), "Object", (ControlDate.toString.call(date)));
+  if (date) {
+    if ((Object.prototype.toString.call(date) === "[object Date]") && (isValidDate(date)) && (date instanceof Date)) {
+      //console.log("instance:", date instanceof Date);
+      //console.log(date.toString());
+      let month = date.getMonth();
+      //console.log(month);
+      if (month == 0 || month == 1 || month == 11) return 'winter';
+      else if (month == 2 || month == 3 || month == 4) return 'spring';
+      else if (month == 5 || month == 6 || month == 7) return 'summer'
+      else return 'autumn';
+    } else return new NotImplementedError('Invalid date!');
 
+  } else return 'Unable to determine the time of year!'
+}
+let input = new Date(1994, 8, 26, 3, 0, 11, 500);
+console.log(getSeason(input));
 module.exports = {
   getSeason
-};
+}
+// npm run test ./test/what* 
