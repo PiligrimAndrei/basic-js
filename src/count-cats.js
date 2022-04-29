a0cac3f6f12a6a16967071ee688762880cc7a555
Andrei Krasko <andrei@krasko.by>
2022-04-29 11:54:53 +0300

    feat: add count

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  /*let str = JSON.stringify(matrix);
+  console.log(str);
+  let newstr = [...str].filter(i => (i == '^')).join('');
+  console.log(newstr);
+  let count = newstr.replace(/[^^]/g, '').length;
+  return count / 2;*/
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    //console.log('длинна', matrix.length)
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == '^^') {
+        count++;
+        //console.log(count);
+      }
+    }
+  }
+  return count;
 }
-
+/*console.log(countCats([
+  [0, 1, '^^'],
+  [0, '^^', 2],
+  ['^^', 1, 2]
+]))*/
 module.exports = {
   countCats
 };
