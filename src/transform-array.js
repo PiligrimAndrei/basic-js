3886f7571c7a381d6283ad338707f6d2df83209e
Andrei Krasko <andrei@krasko.by>
2022-04-29 10:55:03 +0300

    feat: add transform*

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (arr instanceof Array) {
+    let transform = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == '--double-next' || arr[i] == '--discard-prev' || arr[i] == '--discard-next' || arr[i] == '--double-prev') {
+        switch (arr[i]) {
+          case '--double-next':
+            if (typeof arr[i + 1] == 'number') {
+              transform.push(arr[i + 1]);
+            }
+            break;
+          case '--double-prev':
+            if (typeof arr[i - 1] == 'number' && arr[i - 2] != '--discard-next') {
+              transform.push(arr[i - 1]);
+            }
+            break;
+          case '--discard-prev':
+            if (typeof arr[i - 1] == 'number') {
+              let findInd = transform.indexOf(arr[i - 1]);
+              transform.splice(findInd, 1);
+            }
+            break;
+          case '--discard-next':
+            if (typeof arr[i + 1] == 'number') {
+              i += 2
+            }
+            break;
+        }
+      } else transform.push(arr[i]);
+    } return transform;
+  } else throw new NotImplementedError('\'arr\' parameter must be an instance of the Array!')
 }
 
 module.exports = {
