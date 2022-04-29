32b414dbe4c60a23bfc19388dd96fe5b2fab8451
Andrei Krasko <andrei@krasko.by>
2022-04-29 12:46:47 +0300

    feat: add carbon*

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  //console.log(sampleActivity, Number(sampleActivity))
+  let age = Number(sampleActivity);
+  //console.log(age, Number.isInteger(age));
+  if (typeof sampleActivity == 'string' && age < MODERN_ACTIVITY && age > 0) {// && Number.isInteger(age)) {
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+    //console.log(t);
+    return t;
+  } else return false
+
 }
+//console.log(dateSample('5'));
 
 module.exports = {
   dateSample
