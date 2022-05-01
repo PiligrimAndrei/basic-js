857e1165a70ce764790eca611c0d509b9eb59619
Andrei Krasko <andrei@krasko.by>
2022-05-01 19:48:22 +0300

    feat: OK mine*

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  //throw new NotImplementedError('Not implemented');
+  const resultMatrix = JSON.parse(JSON.stringify(matrix))
+  let sum, startI, startJ, endI, endJ = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i == 0) startI = 0;
+      else startI = i - 1;
+      if (j == 0) startJ = 0;
+      else startJ = j - 1;
+      if (i == matrix.length - 1) endI = matrix.length - 1;
+      else endI = i + 1;
+      if (j == matrix[i].length - 1) endJ = matrix[i].length - 1;
+      else endJ = j + 1;
+      console.log(startI, endI, startJ, endJ);
+      sum = 0;
+      for (let k = startI; k <= endI; k++) {
+        for (let l = startJ; l <= endJ; l++) {
+          //console.log('k', k, 'l', l, 'i', i, 'j', j, 'matrix[k][l]', matrix[k][l]);
+          if (matrix[k][l] == true) {
+            sum++;
+            //console.log('sum', sum)
+          }
+        }
+      }
+      resultMatrix[i][j] = sum - 1 * matrix[i][j];
+      sum = 0;
+    }
+  }
+  return resultMatrix;
 }
+
+
+/*console.log(minesweeper([
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]
+]));*/
+
 
 module.exports = {
   minesweeper
 };
+//node ./src/mine-sweeper.js
